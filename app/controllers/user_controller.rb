7b83b603421c6975673d08e8f927ae98dd70bc71
Nicolas Marini <nmarini44@gmail.com>
2018-12-24 22:11:39 +0000

    added get and post request for /signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,26 @@
 class UserController < ApplicationController
-end 
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect "/reviews"
+    end
+  end
+
+  post '/singup' do
+    if logged_in?
+      redirect "/signup"
+    else
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect "/singup"
+      else
+        @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        login(@user.id)
+        redirect "/reviews"
+      end
+    end
+  end
+
+end
