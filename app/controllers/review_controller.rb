79c7371bc7d6f483f809f3cb2ad40eb2da3163c9
Nicolas Marini <nmarini44@gmail.com>
2018-12-29 18:22:42 +0000

    added post /reviews - if statment creating a review or dredirecting back to reviews/new

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -17,6 +17,15 @@ class ReviewController < ApplicationController
     end
   end
 
+  post '/reviews' do
+    if params[:name] != ""
+      @review = Review.create(:name => params[:name], :description => params[:description], :star_rating => params[:star_rating], :user_id => current_user.id)
+      redirect "/reviews/#{@review.id}"
+    else
+      redirect "/reviews/new"
+    end
+  end
+
 
 
 
